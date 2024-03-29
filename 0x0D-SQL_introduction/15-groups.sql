538f1a8fb915543ad955ecde5ef5fef21f6dfee2
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-03-28 19:45:56 -0700

    link

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Count the number of records with the same score and display the score along with the count
 SELECT score, COUNT(*) AS number
-FROM hbtn_0c_0.second_table
+FROM second_table
 GROUP BY score
 ORDER BY number DESC;
