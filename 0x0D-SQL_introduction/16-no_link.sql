bc0bb97e35bca08667f71bbd28baf92345304d75
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-03-28 19:15:11 -0700

    link

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
--- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- script that lists all records of the table second_table of the 
+-- database hbtn_0c_0 in your MySQL server
+
 SELECT score, name
 FROM hbtn_0c_0.second_table
 WHERE name IS NOT NULL
