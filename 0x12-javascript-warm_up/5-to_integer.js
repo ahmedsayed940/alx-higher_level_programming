fdfbf9bb941bf1a7d61adab49a06ea7936953a57
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-04-23 05:29:38 -0700

    javas6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const [,, arg] = process.argv;
 const num = parseInt(arg);
 if (isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
 }
