585c61eabb1d28d44f626f08e3e7522b42b14fbc
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-09 12:50:36 -0700

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept
-curl -s -i -X OPTIONS "$1" | grep "Allow:" | cut -d ":" -f 2 | tr -d '[:space:]'
+#curl -s -i -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
+curl -sI "$1" | grep -i "Allow" | cut -d " " -f 2-
