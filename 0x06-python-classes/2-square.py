46bf0bf92cbaac1e48e2c4314fa4e2aabbc6f6f0
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-09 05:04:34 -0700

    class-2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ empty class Square """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """ attribute:
             size: size os squara.
         """
