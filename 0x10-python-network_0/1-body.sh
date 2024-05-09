758ff2530c0505f8ece85cdfda67be8c9a1ab6e9
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-09 12:24:33 -0700

    0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #sends a GET request to the URL, and displays the body of the response
-# Display only body of a 200 status code response
 curl -s -w "\n%{http_code}" "$1" | awk 'END {if ($1 == 200) print}'
