62ceea4b7e87474992c1897d2b53bb02db87ec38
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-09 04:46:42 -0700

    class-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" empty class Square that defines a square """
+
+
+class Square:
+    """ empty class Square """
+    def __init__(self, size):
+        """ attribute:
+            size: size os squara.
+        """
+        self.__size = size
