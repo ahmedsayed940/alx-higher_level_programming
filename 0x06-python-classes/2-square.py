2a50d355749beef8364d57958b38d23867a3acd1
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-09 05:02:58 -0700

    class-2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" empty class Square that defines a square """
+
+
+class Square:
+    """ empty class Square """
+    def __init__(self, size):
+        """ attribute:
+            size: size os squara.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
